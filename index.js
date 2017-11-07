bf14f60d221d36b6bea6baf5afd09af55878b920
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-07 17:59:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser="Whatever Name";
 const welcomeMessage="Welcome to Flatbook, "+ currentUser + "!";
-const excitedWelcomeMessage="WELCOME TO FLATBOOK, "+"currentUser"+'!';
+const excitedWelcomeMessage=welcomeMessage.toUpperCase();
