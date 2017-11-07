e8d547b384e5363b497915744f8a3a1363c9c6a0
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-07 18:01:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser="Whatever Name";
 const welcomeMessage="Welcome to Flatbook, "+ currentUser + "!";
 const excitedWelcomeMessage=welcomeMessage.toUpperCase();
-const shortGreeting="Welcome, "
+const shortGreeting="Welcome, " +"!"
